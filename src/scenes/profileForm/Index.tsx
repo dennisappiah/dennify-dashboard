e6cec357a86bc8d677fa52880919877fc755996f
dennisappiahALX <appiahdennis1998@gmail.com>
2023-04-07 17:45:42 +0000

    progress circle and stat box

--- src/scenes/profileForm/Index.tsx
+++ src/scenes/profileForm/Index.tsx
@@ -66,7 +66,7 @@ const ProfileForm = () => {
               gap="30px"
               gridTemplateColumns="repeat(4, minmax(0, 1fr))"
               sx={{
-                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                "& > div": { gridColumn: isNonMobile ? undefined : "span 2" },
               }}
             >
               <TextField
