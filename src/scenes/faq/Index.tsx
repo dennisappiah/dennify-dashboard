d9bd97197c83bec3d21ba5f647f7eda188251baf
dennisappiahALX <appiahdennis1998@gmail.com>
2023-04-07 11:35:38 +0000

    faq page

---
+++ src/scenes/faq/Index.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import { tokens } from "../../theme";
+import Header from "../../components/Header";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const FAQ = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  const [expanded, setExpanded] = useState<string | false>(false);
+
+  const handleChange = (isExpanded: boolean, panel: string) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div style={{ margin: "20px" }}>
+      <Header title="FAQ" subTitle="Frequently Asked Questions Page" />
+      {/* One */}
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel1")}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      {/* Two */}
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel2")}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      {/* three */}
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel3")}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Your Favorite Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      {/* four */}
+      <Accordion
+        expanded={expanded === "panel4"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel4")}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Some Random Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      {/* five */}
+      <Accordion
+        expanded={expanded === "panel5"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel5")}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            The Final Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
+
+export default FAQ;
